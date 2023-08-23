605f61e4521a978b9e4d705c2b094e2b975a389d
Shinhyosung <tlsgytjd0218@naver.com>
2023-08-23 14:53:54 +0900

    update

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,19 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+    #read
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
+
+    # create
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+
+    # delete
+    path('<int:id>/delete/', views.delete, name='delete'),
+
+    # update
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/update/', views.update, name='update'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
@@ -20,3 +20,51 @@ def detail(request, id):
     }
 
     return render(request, 'detail.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # post = Post()
+    # post.title = title
+    # post.content = content
+    # post.save()
+
+    post = Post(title=title, content=content)
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
+
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+
+    return redirect('posts:index')
+
+
+def edit(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'edit.html', context)
+
+
+def update(request, id):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post = Post.objects.get(id=id)
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
